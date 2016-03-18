d47613152fc17bc0bd53b69f66afe465339e94e0
Vivian Chen <vivian.vc.chen@gmail.com>
2016-03-18 14:43:05 -0700

    modify

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-// TODO
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      done: false
+    };
+  }
+
+  onListItemHover() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+    var style = {
+      textDecoration: this.state.done ? 'bold' : 'none'
+    };
+
+    return (
+      <li style={style} onmouseover={this.onListItemHover.bind(this)}>{this.props.item}</li>
+    );
+  }
+
+
+}
+
+var GroceryListItem = (props) => (
+  <ul>
+    {props.items.map(item) =>
+      <GroceryListItem item={item} /> 
+      )}
+  </ul>
+);
+
+
+
+ReactDOM.render(<App />, document.getElementById("div #app"));
